f400d626909e04682b3f3ab7a007fe26bac8016c
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-12 12:44:16 +0300

    🔥 refactor(config/index.ts): remove SECRET_KEY and CREDENTIALS environment variables as they are not used anymore

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,5 @@ import { Env } from '@constants';
 config({ path: `.env.${process.env.NODE_ENV || Env.Development}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const { NODE_ENV, PORT, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
 export const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
