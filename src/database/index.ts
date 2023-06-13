4f294e93dd6a190e0ac51681ab3b7339b04047e6
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-13 15:37:43 +0300

    🔧 chore(database/index.ts): remove unused options from dbConnection object to simplify code

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,5 @@
 import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
 
 export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
+  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 };
