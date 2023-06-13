cbf9cd90e4baf823b0ea0c484f4c01d0eac6b215
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-13 20:51:07 +0300

    🚀 feat(utils/index.ts): add getDateString function to format date string to a human-readable format using luxon library

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,6 +1,11 @@
+import { DateTime } from 'luxon';
 import { ThemeMode } from '../constants';
 
 export const getThemeMode = () => {
   const query = '(prefers-color-scheme: dark)';
   return window.matchMedia(query).matches ? ThemeMode.Dark : ThemeMode.Light;
+};
+
+export const getDateString = (date: string) => {
+  return DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED);
 };
