46ee1466a3f5f75405471315cda9c7188b6a3903
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-14 00:07:08 +0300

    🎨 style(AddDeviceForm.tsx): improve form layout by using Grid component from Material UI

--- client/src/components/AddDeviceForm.tsx
+++ client/src/components/AddDeviceForm.tsx
@@ -5,7 +5,8 @@ import {
   Select,
   MenuItem,
   InputLabel,
-  FormControl
+  FormControl,
+  Grid
 } from '@mui/material';
 import { Add as AddIcon } from '@mui/icons-material';
 import { addPeripheralDevice } from '../services/gateway.service';
@@ -37,47 +38,61 @@ const AddDeviceForm: React.FC<Props> = ({ serialNumber, onDeviceAdded }) => {
   };
 
   return (
-    <>
-      <TextField
-        label="UID"
-        type="number"
-        value={newDevice.uid}
-        onChange={(e) =>
-          setNewDevice((prevDevice) => ({ ...prevDevice, uid: +e.target.value }))
-        }
-        inputProps={{ min: 0 }}
-      />
-      <TextField
-        label="Vendor"
-        value={newDevice.vendor}
-        onChange={(e) =>
-          setNewDevice((prevDevice) => ({ ...prevDevice, vendor: e.target.value }))
-        }
-      />
-      <FormControl>
-        <InputLabel>Status</InputLabel>
-        <Select
-          value={newDevice.status}
+    <Grid container spacing={2} alignItems="center">
+      <Grid item xs={3}>
+        <TextField
+          label="UID"
+          type="number"
+          fullWidth
+          value={newDevice.uid}
           onChange={(e) =>
-            setNewDevice((prevDevice) => ({ ...prevDevice, status: e.target.value as DeviceStatus }))
+            setNewDevice((prevDevice) => ({ ...prevDevice, uid: +e.target.value }))
           }
+          inputProps={{ min: 0 }}
+        />
+      </Grid>
+      <Grid item xs={4}>
+        <TextField
+          label="Vendor"
+          fullWidth
+          value={newDevice.vendor}
+          onChange={(e) =>
+            setNewDevice((prevDevice) => ({ ...prevDevice, vendor: e.target.value }))
+          }
+        />
+      </Grid>
+      <Grid item xs={3}>
+        <FormControl fullWidth>
+          <InputLabel>Status</InputLabel>
+          <Select
+            value={newDevice.status}
+            onChange={(e) =>
+              setNewDevice((prevDevice) => ({
+                ...prevDevice,
+                status: e.target.value as DeviceStatus
+              }))
+            }
+          >
+            {Object.values(DeviceStatus).map((status) => (
+              <MenuItem key={status} value={status}>
+                {status}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddIcon />}
+          onClick={handleAddDevice}
+          fullWidth
         >
-          {Object.values(DeviceStatus).map((status) => (
-            <MenuItem key={status} value={status}>
-              {status}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Button
-        variant="contained"
-        color="primary"
-        startIcon={<AddIcon />}
-        onClick={handleAddDevice}
-      >
-        Add Device
-      </Button>
-    </>
+          Add Device
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
