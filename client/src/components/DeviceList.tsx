098c2bc7e1cdeaa5e83637699639928574ed9ea8
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-14 00:41:45 +0300

    ✨ feat(DeviceList.tsx): create new component to display a list of devices

---
+++ client/src/components/DeviceList.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  IconButton
+} from '@mui/material';
+import { Delete as DeleteIcon } from '@mui/icons-material';
+import { getDateString } from '../utils';
+import { removePeripheralDevice } from '../services/gateway.service';
+import { Device } from '../interfaces/device.interface';
+
+interface DeviceListProps {
+  devices: Device[];
+  serialNumber: string;
+  onDeviceRemoved: () => void;
+}
+
+const DeviceList: React.FC<DeviceListProps> = ({
+  devices,
+  serialNumber,
+  onDeviceRemoved
+}) => {
+  const handleRemoveDevice = async (uid: number) => {
+    try {
+      await removePeripheralDevice(serialNumber, uid);
+      onDeviceRemoved();
+    } catch (error) {
+      console.error('Error removing peripheral device:', error);
+    }
+  };
+
+  return (
+    <>
+      <Typography variant="h6">Devices:</Typography>
+      {devices.length > 0 ? (
+        <List>
+          {devices.map(({ uid, vendor, status, dateCreated }) => (
+            <ListItem
+              key={uid}
+              style={{ display: 'flex', alignItems: 'center', width: '100%' }}
+            >
+              <ListItemText
+                primary={`UID: ${uid}`}
+                secondary={`Vendor: ${vendor}`}
+                style={{ flex: 1 }}
+              />
+              <ListItemText
+                primary={`Date created: ${getDateString(dateCreated)}`}
+                secondary={`Status: ${status}`}
+                style={{ flex: 1 }}
+              />
+              <IconButton
+                color="error"
+                onClick={() => handleRemoveDevice(uid)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItem>
+          ))}
+        </List>
+      ) : (
+        <Typography>No devices associated with this gateway.</Typography>
+      )}
+    </>
+  );
+};
+
+export default DeviceList;
--- client/src/components/GatewayList.tsx
+++ client/src/components/GatewayList.tsx
@@ -4,18 +4,15 @@ import {
   AccordionDetails,
   AccordionSummary,
   CircularProgress,
-  Container, Divider,
-  IconButton,
-  List,
-  ListItem,
-  ListItemText,
+  Container,
+  Divider,
   Typography
 } from '@mui/material';
-import { Delete as DeleteIcon, ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
-import { getAllGateways, removePeripheralDevice } from '../services/gateway.service';
+import { ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
+import { getAllGateways } from '../services/gateway.service';
 import { Gateway } from '../interfaces/gateway.interface';
-import { getDateString } from '../utils';
 import AddDeviceForm from './AddDeviceForm';
+import DeviceList from './DeviceList';
 
 const GatewayList: React.FC = () => {
   const [gateways, setGateways] = useState<Gateway[]>([]);
@@ -39,15 +36,6 @@ const GatewayList: React.FC = () => {
       });
   };
 
-  const handleRemoveDevice = async (serialNumber: string, uid: number) => {
-    try {
-      await removePeripheralDevice(serialNumber, uid);
-      fetchGateways();
-    } catch (error) {
-      console.error('Error removing peripheral device:', error);
-    }
-  };
-
   return (
     <Container>
       {loading ? (
@@ -61,33 +49,11 @@ const GatewayList: React.FC = () => {
             <AccordionDetails>
               <Typography variant="subtitle1">Serial Number: {serialNumber}</Typography>
               <Typography variant="subtitle1">IP Address: {ip}</Typography>
-              <Typography variant="h6">Devices:</Typography>
-              {devices.length > 0 ? (
-                <List>
-                  {devices.map(({ uid, vendor, status, dateCreated }) => (
-                    <ListItem key={uid} style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-                      <ListItemText
-                        primary={`UID: ${uid}`}
-                        secondary={`Vendor: ${vendor}`}
-                        style={{ flex: 1 }}
-                      />
-                      <ListItemText
-                        primary={`Date created: ${getDateString(dateCreated)}`}
-                        secondary={`Status: ${status}`}
-                        style={{ flex: 1 }}
-                      />
-                      <IconButton
-                        color="error"
-                        onClick={() => handleRemoveDevice(serialNumber, uid)}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </ListItem>
-                  ))}
-                </List>
-              ) : (
-                <Typography>No devices associated with this gateway.</Typography>
-              )}
+              <DeviceList
+                devices={devices}
+                serialNumber={serialNumber}
+                onDeviceRemoved={fetchGateways}
+              />
               <Divider variant="middle" />
               <br />
               <AddDeviceForm serialNumber={serialNumber} onDeviceAdded={fetchGateways} />
