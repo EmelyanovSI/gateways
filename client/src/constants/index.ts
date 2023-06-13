da4bd561f66639560b9f7cc121b1a518f449ab2c
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-13 18:55:33 +0300

    ✨ feat(constants/index.ts): add constants for environment, status, theme mode, and device status

---
+++ client/src/constants/index.ts
@@ -0,0 +1,28 @@
+export enum Env {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+  Staging = 'staging'
+}
+
+export enum Status {
+  Idle,
+  Loading,
+  Succeeded,
+  Failed
+}
+
+export enum ThemeMode {
+  Light = 'light',
+  Dark = 'dark'
+}
+
+export enum DeviceStatus {
+  Online = 'online',
+  Offline = 'offline'
+}
+
+export const {
+  NODE_ENV = Env.Development,
+  REACT_APP_API_URI
+} = process.env;
